ea7390aec981e00b15cafa7b75273028656723f5
matheus-lagreca <matheus.lagreca@gmail.com>
2022-06-10 18:04:23 -0300

    Fixed env usage of env file

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import App from "./app";
-import env from "../.env";
+import dotenv from "dotenv";
 
+dotenv.config({ path: '../.env' });
 
-const hostname = env.HOSTNAME;
-const port = env.PORT;
+const hostname = process.env.HOSTNAME;
+const port = process.env.PORT;
+
 
 App.server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
